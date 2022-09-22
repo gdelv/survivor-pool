9b0cb319c1a80719fda0946601d63dbd0b567d18
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2022-09-22 10:54:58 -0400

    added rules

--- src/App.js
+++ src/App.js
@@ -49,6 +49,13 @@ function App() {
       </div>
 <br></br>
 <h1 className="align-center">Weekly Picks</h1>
+<h2 className="align-center">Rules</h2>
+<ul className="align-center">
+<p>Each pool member chooses one NFL team each week they think will WIN  their game outright (picks are made 'straight up', not using a point spread system).</p>
+<p>NFL teams can only be picked once during the season.</p>
+<p>If a game your picked team is playing in results in a tie, your pick will be correct and your entry will advance.</p>
+<p>The final pick deadline each week is Sunday at 1pm ET. If you want to pick an earlier game you must do so by kickoff of that game.</p>
+</ul>
       <table style={{ width: "85%", height: "60vh", margin: "auto" }}>
         <tr>
           <th></th>
